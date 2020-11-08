379b882590433e415f9d65cb2062912f41047dd5
Mihir Gautam <Mihir Gautam@DESKTOP-JPP42H1>
2020-11-08 21:15:29 +0530

    [Mihir] Add. New database for address book created

---
+++ AddressBookUsingLinq/AddressBook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace AddressBookUsingLinq
+{
+    class AddressBook
+    {
+        /// <summary>
+        /// UC1 New empty database for address book created;
+        /// </summary>
+        DataTable dataTable = new DataTable() { };
+    }
+}
